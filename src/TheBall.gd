c31d11c3160970b242eb429ecfba50cbca66e36b
Danil Efimov <danil.efimov.295@mail.ru>
2022-04-25 18:40:52 +0300

    Add destructible blocks as tiles

--- src/TheBall.gd
+++ src/TheBall.gd
@@ -1,30 +1,44 @@
 extends KinematicBody2D
 
-const theBallSpeed = 3
+const theBallSpeed = 250
 var velocity = Vector2(theBallSpeed, -theBallSpeed)
+var tilemap
 
 func _physics_process(delta):
-	var collision = move_and_collide(velocity, true)
-#	var collision = get_last_slide_collision()
+	var collision = move_and_collide(velocity*delta, true)
 	if collision:
-		print(collision)
+		print(collision.collider)
 		velocity = velocity.bounce(collision.normal)
-		var tile_pos = collision.collider.world_to_map(position)
-		# Find the colliding tile position
-		tile_pos -= collision.normal
-		# Get the tile id
-		var tile_id = collision.collider.get_cellv(tile_pos)
-		var tile_name = collision.collider.tile_set.tile_get_name(tile_id)
-		print(tile_name)
+		if collision.collider.is_in_group("walls"):
+			var tile_pos = collision.collider.world_to_map(position) - collision.normal
+			# Get the tile id
+			var tile_id = collision.collider.get_cellv(tile_pos)
+			var tile_name = collision.collider.tile_set.tile_get_name(tile_id)
+#			print(tile_name)
+			pass
+		if collision.collider.is_in_group("blocks"):
+			hitBlock(collision)
+
+func hitBlock(collision:KinematicCollision2D):
+#	print(tilemap)
+	tilemap = collision.collider
+	var cell = tilemap.world_to_map(collision.position - collision.normal)
+	var tileId = tilemap.get_cellv(cell)
+	print(cell)
+	print(tileId)
+	if tilemap == $"../Block_2hp":
+		if tileId == 0:
+			tilemap.set_cellv(cell, 1)
+		if tileId == 1:
+			tilemap.set_cellv(cell, -1)
+	if tilemap == $"../Block_3hp":
+		if tileId == 0:
+			tilemap.set_cellv(cell, 1)
+		if tileId == 1:
+			tilemap.set_cellv(cell, 2)
+		if tileId == 2:
+			tilemap.set_cellv(cell, -1)
 
 func _ready():
-	pass # Replace with function body.
-
-#func _process(delta):
-#
-#
-#	for i in get_slide_count():
-#		var collision = get_slide_collision(i)
-#		if collision.collider is TileMap:
-#			print(velocity)
-#	position += theBallSpeed * delta * direction
+	pass
+	
